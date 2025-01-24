8ceabd0c34a6f1f4839ca72d122aff5722ccf840
aruyaohi <omariontechnologies@gmail.com>
2025-01-24 18:50:41 +0100

    made some changes

--- airplug/src/app/dashboard/page.tsx
+++ airplug/src/app/dashboard/page.tsx
@@ -27,12 +27,12 @@ export default function Dashboard() {
         </div>
         {/* Menu Button */}
         <button
-          className="lg:hidden p-2 border-2 border-gray-500 rounded-md focus:outline-none transition-transform"
+          className="lg:hidden p-2 border-2 border-gray-600 rounded-md focus:outline-none transition-transform"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 text-gray-500"
+            className="h-6 w-6 text-gray-300"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -89,7 +89,7 @@ export default function Dashboard() {
           <div>
             <label className="block text-sm font-semibold text-gray-700">Network Providers</label>
             <div className="flex justify-between items-center gap-3">
-              <button className="flex-1 py-3 border-2 border-gray-300 rounded-md hover:bg-[#3de25e] hover:border-none transition-all duration-300 flex items-center justify-center">
+              <button className="flex-1 py-3 border-2 border-[#f1952c] rounded-md hover:bg-[#3de25e] hover:border-none transition-all duration-300 flex items-center justify-center">
                 <Image src="/images/glo.png" alt="GLO" width={40} height={40} />
               </button>
               <button className="flex-1 py-3 border-2 border-gray-300 rounded-md hover:bg-yellow-500 hover:border-none  transition-all duration-300 flex items-center justify-center">
@@ -188,9 +188,9 @@ export default function Dashboard() {
         </select>
       </div>
 
-      {/* Proceed Button 
+      {/* Proceed Button */}
       <div className="p-6">
-        <button className="w-full bg-[#030303] text-white py-3 rounded-md font-semibold">
+        <button className="w-full bg-[#f1952c] text-white py-3 rounded-md font-semibold">
           Proceed
         </button>
       </div>
